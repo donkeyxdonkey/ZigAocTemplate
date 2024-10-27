0addf44441e5638e03be4fa87b32886c9ed21131
McNugget <alexd84a@gmail.com>
2024-10-27 11:07:08 +0100

    x

--- src/days/day01.zig
+++ src/days/day01.zig
@@ -7,35 +7,8 @@ const print = std.debug.print;
 pub const Day01 = struct {
     pub fn part1(self: *Day01, input: *const []const u8, debug_print: bool) e.DayError![]const u8 {
         _ = &self;
+        _ = &input;
         _ = &debug_print;
-
-        var lines = std.mem.splitSequence(u8, input.*, "\n");
-        var result: u32 = 0;
-
-        // const target: i32 = 2020;
-
-        outer: while (lines.next()) |line| {
-            while (lines.next()) |inner| {
-                if (std.mem.eql(u8, line, inner)) {
-                    continue;
-                }
-
-                break :outer;
-                // const quacki = try std.fmt.parseInt(i32, line, 10);
-                // const quacky = try std.fmt.parseInt(i32, line, 10);
-
-                // if (quacki + quacky == target) {
-                //     print("{d}\n", .{quacki});
-                //     print("{d}\n", .{quacky});
-                //     break :outer;
-                // }
-            }
-            result += 1;
-            print("{} result {s}\n", .{ result, line });
-        }
-
-        if (debug_print)
-            print("{s}\n", .{input.*});
 
         return "Day1 part1";
     }
